add6d14ad5247f79e6aaed7ad517c8eb3eb35fc9
zaurmagomedov <mza97@mail.ru>
2022-02-28 10:29:12 +0300

    Создал логику обработки аватара путем наложения на него водяного знака.

--- DatingSite/main/models.py
+++ DatingSite/main/models.py
@@ -7,13 +7,15 @@ class User(AbstractUser):
         ('MALE', 'Male'),
         ('FEMALE', 'Female'),
     )
-
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
     gender = models.CharField(max_length=20, choices=GENDER_SELECTION)
     email = models.EmailField(unique=True)
 
     avatar = models.ImageField(upload_to='media/', blank=True)
+    username = None
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.email}'
--- DatingSite/main/serializers.py
+++ DatingSite/main/serializers.py
@@ -1,9 +1,51 @@
+from io import BytesIO
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.password_validation import validate_password
+from PIL import Image, ImageEnhance
 from django.core.files import File
 
 BASIC_AVATAR = 'media/basic_avatar.jpeg'
+WATERMARK = 'main/static/main/img/water.png'
+
+
+def generate_avatar_with_watermark(avatar):
+    original_file = avatar
+
+    image = Image.open(avatar)
+    watermark = Image.open(WATERMARK)
+    image_with_watermark = add_watermark(image, watermark)
+
+    buffer = BytesIO()
+    format_ = original_file.content_type.split('/')[-1]
+    image_with_watermark.save(buffer, format_)
+    converted_file = InMemoryUploadedFile(
+        buffer,
+        original_file.field_name,
+        original_file.name,
+        original_file.content_type,
+        buffer.tell(),
+        None
+    )
+
+    return converted_file
+
+
+def add_watermark(image, watermark, opacity=1):
+    assert 0 <= opacity <= 1
+    if opacity < 1:
+        if watermark.mode != 'RGBA':
+            watermark = watermark.convert('RGBA')
+        else:
+            watermark = watermark.copy()
+        alpha = watermark.split()[3]
+        alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+        watermark.putalpha(alpha)
+    layer = Image.new('RGBA', image.size, (0, 0, 0, 0))
+    layer.paste(watermark, (1, 1))
+    return Image.composite(layer, image, layer)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -33,6 +75,8 @@ class UserSerializer(serializers.ModelSerializer):
         if user_didnt_send_avatar:
             file = open(BASIC_AVATAR, 'rb')
             validated_data['avatar'] = File(file)
+        else:
+            validated_data['avatar'] = generate_avatar_with_watermark(validated_data['avatar'])
 
         user = User.objects.create(
             email=validated_data['email'],
