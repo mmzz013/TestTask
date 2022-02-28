a27a724e263371eab2255049dfbb289ba7d62b97
zaurmagomedov <mza97@mail.ru>
2022-02-28 10:04:16 +0300

    Создал проект и модель User в нём.

---
+++ DatingSite/main/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    GENDER_SELECTION = (
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+    )
+
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    gender = models.CharField(max_length=20, choices=GENDER_SELECTION)
+    email = models.EmailField(unique=True)
+
+    avatar = models.ImageField(upload_to='media/', blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.email}'
